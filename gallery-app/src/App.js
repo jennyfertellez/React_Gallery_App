fce76eade41830cbf4a18ffb849e738bbb440fbc
jennyfertellez <jennyfertellez@gmail.com>
2022-11-28 20:23:00 -0800

    Updated navigation and created file for not found

--- gallery-app/src/App.js
+++ gallery-app/src/App.js
@@ -1,16 +1,21 @@
+//Required Dependencies
 import React, { useEffect, useState } from "react";
 import axios from "axios"
 import apiKey from './config'
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  const [photo, setPhoto] = useState([]);
+//Components
+
+
+//Main App Component
+function App = (props) => {
+  const [photos, setPhotos] = useState([]);
   useEffect(() => {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&per_page=24&format=json&nojsoncallback=1`)
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${keyword}&per_page=24&format=json&nojsoncallback=1`)
       .then(response => {
         // handle success
-        setPhoto(response.data.data)
+        setPhotos(response.data.data)
 
       })
       .catch(error => {
--- gallery-app/src/components/Navigation.js
+++ gallery-app/src/components/Navigation.js
@@ -1,1 +1,22 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li>
+                    <NavLink to="/bunny">Bunny</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/kitten">Kitten</NavLink>
+                </li>
+                <li>
+                    <NavLink to="llama">Llama</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
