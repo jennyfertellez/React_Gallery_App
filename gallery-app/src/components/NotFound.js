e6592c275b3999046d1e3c61b13329b810d6862f
jennyfertellez <jennyfertellez@gmail.com>
2022-11-28 20:27:23 -0800

    Update NotFound.js

---
+++ gallery-app/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = (props) => {
+    return (
+        <li className="not-found">
+            <h3>Sorry. No Results Found.</h3>
+            <p>Please Try A New Search or Refresh Browser.</p>
+        </li>
+    );
+};
+
+export default NotFound;
